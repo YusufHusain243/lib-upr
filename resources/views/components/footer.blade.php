d007d39fad625dc21f95c906e98ab1e6b5cafac6
YusufHusain243 <husainyusuf382@gmail.com>
2023-04-09 10:42:58 +0700

    repair footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -10,32 +10,32 @@
                     73111, Kalimantan Tengah
                 </p>
 
-                <a href="">
+                <p>
                     email: info@upr.ac.id
-                </a>
+                </p>
             </div>
             <div class="col-lg-5 py-3">
                 <h5>WEBSITES UPR</h5>
                 <ul class="footer-menu">
-                    <li><a href="#">FAKULTAS KEGURUAN DAN ILMU PENDIDIKAN</a></li>
-                    <li><a href="#">FAKULTAS EKONOMI</a></li>
-                    <li><a href="#">FAKULTAS PERTANIAN</a></li>
-                    <li><a href="#">FAKULTAS TEKNIK</a></li>
-                    <li><a href="#">FAKULTAS HUKUM</a></li>
-                    <li><a href="#">FAKULTAS ILMU SOSIAL DAN POLITIK</a></li>
-                    <li><a href="#">FAKULTAS KEDOKTERAN</a></li>
-                    <li><a href="#">FAKULTAS MATEMATIKA DAN ILMU PENGTAHUAN ALAM</a></li>
-                    <li><a href="#">PROGRAM PASCA SARJANA</a></li>
+                    <li><a target="_blank" href="https://fkip.upr.ac.id/">FAKULTAS KEGURUAN DAN ILMU PENDIDIKAN</a></li>
+                    <li><a target="_blank" href="https://feb.upr.ac.id/">FAKULTAS EKONOMI</a></li>
+                    <li><a target="_blank" href="https://faperta.upr.ac.id/">FAKULTAS PERTANIAN</a></li>
+                    <li><a target="_blank" href="https://ft.upr.ac.id/">FAKULTAS TEKNIK</a></li>
+                    <li><a target="_blank" href="https://fh.upr.ac.id/">FAKULTAS HUKUM</a></li>
+                    <li><a target="_blank" href="https://fisip.upr.ac.id/">FAKULTAS ILMU SOSIAL DAN POLITIK</a></li>
+                    <li><a target="_blank" href="https://medical.upr.ac.id/">FAKULTAS KEDOKTERAN</a></li>
+                    <li><a target="_blank" href="https://fmipa.upr.ac.id/">FAKULTAS MATEMATIKA DAN ILMU PENGTAHUAN ALAM</a></li>
+                    <li><a target="_blank" href="https://pps.upr.ac.id/">PROGRAM PASCA SARJANA</a></li>
                 </ul>
             </div>
             <div class="col-lg-3 py-3">
                 <h5>UNIT PELAYANAN TEKNIS & LEMBAGA</h5>
                 <ul class="footer-menu">
-                    <li><a href="#">UPT. TIK</a></li>
-                    <li><a href="#">UPT. BAHASA</a></li>
-                    <li><a href="#">LPPM</a></li>
-                    <li><a href="#">LP3MP</a></li>
-                    <li><a href="#">CIMTROP</a></li>
+                    <li><a target="_blank" href="https://tik.upr.ac.id/">UPT. TIK</a></li>
+                    <li><a target="_blank" href="https://uptbahasaupr.wordpress.com/">UPT. BAHASA</a></li>
+                    <li><a target="_blank" href="https://lppm.upr.ac.id/">LPPM</a></li>
+                    <li><a target="_blank" href="https://lp3mp.upr.ac.id/sejarah/">LP3MP</a></li>
+                    <li><a target="_blank" href="https://cimtrop.upr.ac.id/">CIMTROP</a></li>
                 </ul>
             </div>
         </div>
